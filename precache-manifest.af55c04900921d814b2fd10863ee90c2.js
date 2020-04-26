335c75692f3bfa1fb52c220a715a846d77afda5d
Michelle Pham <m_vpham@M-vpham.local>
2020-04-26 16:12:39 -0400

    Updates

---
+++ precache-manifest.af55c04900921d814b2fd10863ee90c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18476875b6aac2de7936ad7a06b8549",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "ecbfdd7ec4e906e86482",
+    "url": "/pokedex/static/css/main.b6946562.chunk.css"
+  },
+  {
+    "revision": "2ceaaf29f9804dbe25db",
+    "url": "/pokedex/static/js/2.d8a638d6.chunk.js"
+  },
+  {
+    "revision": "ecbfdd7ec4e906e86482",
+    "url": "/pokedex/static/js/main.7934ad6e.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
